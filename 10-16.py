feeef719db41de0024567de1eba7ebc66ece6fdd
William-Anderson2020 <william.anderson@sitechhs.com>
2020-10-17 17:03:09 -0400

    Finished 10-16

--- 10-16.py
+++ 10-16.py
@@ -13,3 +13,42 @@ For example, given the input [2, 1, 2, 3, 3, 1, 3, 5], the longest portion will
 involve types 1 and 3, with a length of four.
 
 """
+
+def twoTypeLen(path):
+    path.append("")
+    checkedA = []
+    count = 0
+    types = []
+    curS = []
+    for t1 in path:
+        if t1 not in checkedA:
+            checkedB = []
+            for t2 in path:
+                if t1 != t2 and t2 not in checkedB:
+                    subCount = 0
+                    aString = []
+                    for a in path:
+                        aString.append(a)
+                        if a == t1 or a == t2:
+                            subCount += 1
+                            curS.append(a)
+                        else:
+                            if subCount > count:
+                                count = subCount
+                                types = [t1,t2]
+                            subCount = 0
+                            curS = []
+                    #print(aString)
+                    checkedB.append(t2)
+        checkedA.append(t1)
+        
+    return count
+                    
+
+testP = [2, 1, 2, 3, 3, 1, 3, 5, 3, 5, 3]
+print(twoTypeLen(testP))
+            
+            
+            
+            
+            
